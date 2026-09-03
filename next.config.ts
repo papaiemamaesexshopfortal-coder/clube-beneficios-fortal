02d8697ecae1c410c32866e6becc7858e708f905
papaiemamaesexshopfortal-coder <papaiemamaesexshopfortal@gmail.com>
2026-09-03 20:18:18 -0300

    feat: integrate Sentry with Next.js build

---
+++ next.config.ts
@@ -0,0 +1,6 @@
+import { withSentryConfig } from '@sentry/nextjs';
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = { reactStrictMode: true };
+
+export default withSentryConfig(nextConfig, { org: process.env.SENTRY_ORG, project: process.env.SENTRY_PROJECT, silent: true, widenClientFileUpload: true });
